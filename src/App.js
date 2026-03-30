7a3ef3b50c2285776f79f9f803f78886559b439c
william72425 <125339420+william72425@users.noreply.github.com>
2026-03-30 17:13:21 +0630

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{textAlign:'center', padding:'50px'}}>
+      <h1>🌟 Aura System</h1>
+      <p>Deployment Successful!</p>
+    </div>
+  );
+}
+export default App;
